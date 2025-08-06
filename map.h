c6ce428701ba6ba138e00b62bf30013986bd94f6
Vyom Budha <164761373+Vyombudha@users.noreply.github.com>
2025-08-06 12:32:43 +0545

    Update map.h

--- map.h
+++ map.h
@@ -1,17 +1,19 @@
 #ifndef MAP_H
 #define MAP_H
 
-// Map myMap<INT,STRING> = Map<>() ;
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <stdbool.h>
 
-const char NO_VALUE = '\0';
-const size_t MAX_STRING_BUFFER = 257;
-const size_t INITIAL_SIZE = 157;
+#define NO_VALUE_CHAR '\0'
+#define NO_VALUE_INT -99099
+#define NO_VALUE_FLOAT -99099.0f
+#define NO_VALUE_STRING "NO_VALUE_STRING"
+
+#define MAX_STRING_BUFFER 257
+#define INITIAL_SIZE 1024
 
 typedef enum
 {
@@ -30,58 +32,16 @@ typedef enum
     MAP_VALUE_NULL_POINTER = 4,
     MAP_KEY_NULL_POINTER = 5,
     MAP_NULL_POINTER = 6,
-    MAP_FULL = 7,
-    MAP_PRINTING_SUCCESSFUL = 8,
-    MAP_LINKED_LIST_ALLOCATION_ERROR = 9,
-    MAP_NODE_ALLOCATION_ERROR = 10,
-    OK = 11
+    MAP_PRINTING_SUCCESSFUL = 7,
+    MAP_LINKED_LIST_ALLOCATION_ERROR = 8,
+    MAP_NODE_ALLOCATION_ERROR = 9,
+    MAP_REHASHING_SUCCESSFUL = 10,
+    MAP_LINKED_LIST_NULL_POINTER = 11,
+    OK = 12,
+    MAP_KEY_NOT_FOUND = 13,
 } Repsone_codes;
 
-const char *GET_MESSAGE_CODE[12] = {
-    [MAP_SET_SUCCESSFUL] = "MAP_SET_SUCCESSFUL",
-    [MAP_GET_SUCCESSFUL] = "MAP_GET_SUCCESSFUL",
-    [MAP_VALUE_ALLOCATION_FALIURE] = "MAP_VALUE_ALLOCATION_FALIURE",
-    [MAP_KEY_ALLOCATION_FALIURE] = "MAP_KEY_ALLOCATION_FALIURE",
-    [MAP_VALUE_NULL_POINTER] = "MAP_VALUE_NULL_POINTER",
-    [MAP_KEY_NULL_POINTER] = "MAP_KEY_NULL_POINTER",
-    [MAP_NULL_POINTER] = "MAP_NULL_POINTER",
-    [MAP_FULL] = "MAP_FULL",
-    [MAP_PRINTING_SUCCESSFUL] = "MAP_PRINTING_SUCCESSFUL",
-    [MAP_LINKED_LIST_ALLOCATION_ERROR] = "MAP_LINKED_LIST_ALLOCATION_ERROR",
-    [MAP_NODE_ALLOCATION_ERROR] = "MAP_NODE_ALLOCATION_ERROR",
-    [OK] = "OK"};
-
-const size_t GET_SIZE_OF[4] = {
-    [STRING] = MAX_STRING_BUFFER,
-    [INT] = sizeof(int),
-    [CHAR] = sizeof(char),
-    [FLOAT] = sizeof(float)};
-
-void print_string(void *data);
-void print_int(void *data);
-void print_char(void *data);
-void print_float(void *data);
-
-// Array of function Pointer!!
-void (*GET_PRINT_FUNC[4])(void *) = {
-    [STRING] = print_string,
-    [INT] = print_int,
-    [CHAR] = print_char,
-    [FLOAT] = print_float};
-
-bool are_equal_int(const void *key_to_search, const void *key_in_map);
-bool are_equal_string(const void *key_to_search, const void *key_in_map);
-bool are_equal_char(const void *key_to_search, const void *key_in_map);
-bool are_equal_float(const void *key_to_search, const void *key_in_map);
-
-bool (*GET_ARE_EQUAL_FUNC[4])(const void *, const void *) = {
-    [STRING] = are_equal_string,
-    [INT] = are_equal_int,
-    [CHAR] = are_equal_char,
-    [FLOAT] = are_equal_float};
-
-// This is the definition of Proccess_repsonse Struct and it's newResponse()  and !
-
+// Define ALL structs first before any function declarations
 typedef struct
 {
     int integer;
@@ -96,11 +56,6 @@ typedef struct
     char return_message[MAX_STRING_BUFFER];
     Returning_value value;
 } Proccess_responses;
-
-Proccess_responses new_response(Repsone_codes code, Data_type type_of_value); // will auto generate
-
-// These are the Functions/ Definitions handling Nodes/Map
-// This should return the Data_type Approprite value when map_value_of() is called under Proccess_response
 
 typedef struct Node
 {
@@ -120,32 +75,57 @@ typedef struct
     bool (*are_equal)(const void *, const void *);
     void (*print_key)(const void *);
     void (*print_value)(const void *);
+    void (*assign_value)(Returning_value *, void *);
 } data_methods;
 
 typedef struct
 {
-    Node **linked_lists; //  this is a pointer to a pointer pointing to an Array of Linked_list Nodes; || the Insert function for the Nodes will be Head inserted!
+    Node **linked_lists;
     Data_type type_of_value;
     Data_type type_of_key;
-
     data_methods methods;
-
+    unsigned int (*hash)(const void *, size_t);
     size_t max_size;
     size_t curr_size;
 } Map;
 
+// NOW declare external variables and functions (after struct definitions)
+extern const char *GET_MESSAGE_CODE[14];
+extern const size_t GET_SIZE_OF[4];
+extern void (*GET_PRINT_FUNC[4])(const void *);
+extern bool (*GET_ARE_EQUAL_FUNC[4])(const void *, const void *);
+extern unsigned int (*GET_HASH_FUNC[4])(const void *, size_t);
+extern void (*GET_ASSIGN_VALUE_FUNC[4])(Returning_value *, void *);
+
+// Function declarations
+void print_string(const void *data);
+void print_int(const void *data);
+void print_char(const void *data);
+void print_float(const void *data);
+
+bool are_equal_int(const void *key_to_search, const void *key_in_map);
+bool are_equal_string(const void *key_to_search, const void *key_in_map);
+bool are_equal_char(const void *key_to_search, const void *key_in_map);
+bool are_equal_float(const void *key_to_search, const void *key_in_map);
+
+unsigned int hash_string(const void *key, size_t max_size);
+unsigned int hash_int_or_char(const void *key, size_t max_size);
+unsigned int hash_float(const void *key, size_t max_size);
+
+void assign_value_int(Returning_value *value, void *data);
+void assign_value_char(Returning_value *value, void *data);
+void assign_value_string(Returning_value *value, void *data);
+void assign_value_float(Returning_value *value, void *data);
+
+Proccess_responses new_response(Repsone_codes code);
 Node_return new_node(void *key, void *value, Data_type type_of_key, Data_type type_of_value);
-
-// Map myMap  = new_map(INT,STRING,19) ;
-
+Proccess_responses insert_node_to_list(Node **head, Node *node);
 Map new_map(Data_type type_of_key, Data_type type_of_value);
-
 Proccess_responses map_set(Map *map, void *key, void *value);
-Proccess_responses map_value(const Map *map, void *key); // I MAY HAVE SOME WAY TO RETURN AN APPROPIRATE RETURN VALUE!!
+Proccess_responses map_value(const Map *map, void *key);
 bool map_has(const Map *map, void *key);
-
-Proccess_responses map_print(const Map *map); // will internally call linked_list[index]->print_key()(and)->print_value(); in a LOOP
-Proccess_responses map_destroy(Map *map);     // will internally  free all linked_list[index] Nodes and set linked_lists to NULL!
-Proccess_responses map_rehash(Map *map);      // Will Be used to reHash the map!
+Proccess_responses map_print(const Map *map);
+Proccess_responses map_destroy(Map *map);
+Proccess_responses map_rehash(Map *map);
 
 #endif
